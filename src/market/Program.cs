05b9015cee5f3df0f403af4d84fe36bc9b2b4339
xtasy <2xtasy@gmail.com>
2023-11-12 01:44:19 +0300

    1

--- src/market/Program.cs
+++ src/market/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace market
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace market
 {
-    internal class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
+            var stats = new Statistics();
+            List<Product> listOfProducts = stats.GetFromFile();
+            var listOfCategories = listOfProducts.Select(el => el.Category).Distinct();
+            var listOfDescriptions = listOfProducts.Select(el => el.Description).Distinct();
+            var listOfVendors = listOfProducts.Select(el => el.Vendor).Distinct();
+            
+            Console.WriteLine($"Всего категорий: {listOfCategories.Count()}\nВсего товаров: {listOfDescriptions.Count()}\n" +
+                              $"Всего производителей: {listOfVendors.Count()}");
+
+            var listOfKeywords = stats.GetTopKeywords(listOfProducts, 15);
+
+            //вот тут ручками можно поменять категорию))
+            var cat = listOfCategories.ToArray()[1];
+            Console.WriteLine($"Категория: {cat}");
+
+            foreach (var el in listOfKeywords[cat])
+            {
+                var list = listOfKeywords.Where(value => value.Value.Exists(word => word == el)).
+                    Select(category => category).ToList();
+                if (list.Count >= 5)
+                    Console.WriteLine($"Слово {el} встречается ещё в {list.Count-1} категориях");
+                else 
+                    foreach (var a in list)
+                        if (a.Key != cat)
+                            Console.WriteLine($"Слово {el} встречается в категории {a.Key}");
+            }
         }
     }
 }
--- src/market/Statistics.cs
+++ src/market/Statistics.cs
@@ -1,7 +1,119 @@
-﻿namespace market
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace market
 {
     public class Statistics
     {
+        public List<Product> GetFromFile()
+        {
+            var path = @".\allitems.txt";
+            var list = new List<Product>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var product = new Product
+                    {
+                        Category = line.Split('\t')[0],
+                        Description = line.Split('\t')[1],
+                        Vendor = line.Split('\t')[2]
+                    };
+                    list.Add(product);
+                }
+            }
+            return list;
+        }
+        
+        private List<string> GetProdByCat(List<Product> listProd,string category)
+        {
+            var list = listProd.Where(cat => cat.Category == category).
+                Select(el => el.Description).ToList();
+            return list;
+        }
+
+        private bool isWord(string word)
+        {
+            if (word.Length < 3)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        
+        private List<string> ParseLine(string line)
+        {
+            var list = new List<string>();
+            line = line.ToLower();
+
+            var sb = new StringBuilder();
+            foreach (var ch in line)
+            {
+                if (char.IsLetter(ch) && ch >= 'а' && ch <= 'я')
+                    sb.Append(ch);
+                else sb.Append(' ');
+            }
+
+            list.AddRange(sb.ToString().Split(' '));
+            list.RemoveAll(string.IsNullOrWhiteSpace);
+            return list;
+        }
+
+        private Dictionary<string, int> GetWords(List<string> list)
+        {
+            var dict = new Dictionary<string, int>();
+            
+            for (int i = 0; i < list.Count; i++)
+            {
+                var listWords = ParseLine(list[i]);
+                
+                for (int j = 0; j < listWords.Count; j++)
+                {
+                    if (dict.ContainsKey(listWords[j]))
+                        dict[listWords[j]]++;
+                    else if(isWord(listWords[j])) 
+                        dict.Add(listWords[j], 1);
+                }
+            }
+            return dict;
+        }
+
+        public Dictionary<string, List<string>> GetTopKeywords(List<Product> listOfProducts, int top)
+        {
+            var listOfCategories = listOfProducts.Select(el => el.Category).Distinct();
+            var keywords = new Dictionary<string, List<string>>();
+            
+            foreach (var category in listOfCategories)
+            {
+                List<string> listProdFromCat = GetProdByCat(listOfProducts, category);
+                var dict = GetWords(listProdFromCat);
+                var listWords = dict.Select(el => (el.Key, el.Value)).
+                    OrderBy(el => -el.Value).Take(top).ToList();
+                
+                Console.WriteLine($"{listWords.Count} возможных ключевых слов в категории {category}:");
+
+                var list = new List<string>();
+                if (!keywords.ContainsKey(category))
+                    keywords.Add(category,list);
+
+                foreach (var el in listWords)
+                {
+                    Console.Write(el.Key + " ");
+                    keywords[category].Add(el.Key);
+                }
+                
+                Console.WriteLine("\nДобавлено в коллекцию\n");
+            }
+            
+            return keywords;
+        }
+        
         
     }
 }
